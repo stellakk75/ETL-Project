91132ba4e4df5191b15f46d04bdc8b29bb538bcb
pparashar68 <pparashar68@gmail.com>
2020-12-03 21:25:37 -0800

    postgress tables

---
+++ postgres_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE author( 
+   author_name varchar(50) PRIMARY KEY,    
+   dob varchar(50),    
+   description varchar(30000)
+)
+CREATE TABLE quotes(
+    quote_id INTEGER PRIMARY KEY,    
+    author_name varchar(50),    
+    quote varchar(3500)
+);
+CREATE TABLE tags( 
+   quote_id INTEGER,    
+   tags varchar(32)
+);

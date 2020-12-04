7a7ef4758abf6645378649af9ddb558391a67366
Hanna Lee <sarah@Hannas-MacBook-Pro.local>
2020-12-04 16:11:57 -0500

    flask

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ def index():
     return render_template("index.html", listings=listings)
 
 
-@app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings.drop()
-    listings_data = scrape_craigslist.scrape()
-    listings.insert_many(listings_data)
-    return redirect("/", code=302)
+@app.route("/quotes")
 
+@app.route("/authors")
+
+@app.route("/authors/<author name>")
+
+@app.route("/tags")
+
+@app.route("/tags/<tag>")
+
+@app.route("/top10tags")
 
 if __name__ == "__main__":
     app.run(debug=True)

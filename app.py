05e35985c1422ada61292fd74a0488e3d2184c03
pparashar68 <pparashar68@gmail.com>
2020-12-04 21:29:35 -0800

    Added top10tags code

--- app.py
+++ app.py
@@ -1,29 +1,63 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_craigslist
+# Import Libraries
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import psycopg2
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
+#################################################
 
-# Use flask_pymongo to set up mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
-
-@app.route("/")
-def index():
-    listings = mongo.db.listings.find()
-    return render_template("index.html", listings=listings)
+# Database Setup
+connection = psycopg2.connect(user = "postgres",
+                                  password = "postgres",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "quotes_db")
+db_string = "postgres://postgres:postgres@localhost:5432/quotes_db"
+engine = connection.cursor()
+db = create_engine(db_string)
 
 
 @app.route("/quotes")
+def quotes():
+    engine.execute("SELECT COUNT(*) FROM quotes;")
+    total_tags = engine.fetchone()  
+    quotes_all_df = pd.read_sql_query(
+        '''SELECT quotes.quote_id,quotes.author_name,quotes.quote,tags.tag \
+                    FROM quotes \
+                    JOIN \
+                    tags ON quotes.quote_id = tags.quote_id \
+                    ORDER BY quotes.quote_id''',db
+                                    )
+    
 
-@app.route("/authors")
-
-@app.route("/authors/<author name>")
-
-@app.route("/tags")
-
-@app.route("/tags/<tag>")
-
+    return jsonify(quotes_all_df.values.tolist())
 @app.route("/top10tags")
+def top10tags():
+    top10tags_df = pd.read_sql_query(
+                   ''' SELECT tag,count(*) FROM TAGS \
+                       GROUP BY tag \
+                       ORDER BY count(*) DESC LIMIT 10
+                   ''' , db
+                                    )      
+    return jsonify(top10tags_df.values.tolist())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
